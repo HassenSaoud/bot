3aa85acfecf61b511dd542148e50ee55e1fe8e7e
Hassen Saoud <70983688+HassenSaoud@users.noreply.github.com>
2025-04-14 23:33:16 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -19,16 +19,6 @@ TELEGRAM_CHAT_ID = 5550229154  # your user ID
 bot_running = threading.Event()
 API_KEY = ("aYqlaKxzBqwMIyZUicdVU82UOV5pMQnfgd3u2N8wYuXZJ0nTif63xFFU1eFUU8b9")
 API_SECRET = ("ZO1H8UDSZu7QxcyrXo8jReKh3L82Hm7w3lFGhPbsuUFI4pmVD5NwGrWVxiSLB72W")
-
-try:
-    client = Client(API_KEY, API_SECRET)
-    client.ping()
-except Exception as e:
-    print("[BINANCE ERROR]", e)
-    exit()
-
-print("[DEBUG] API_KEY loaded:", API_KEY[:6], "***")
-print("[DEBUG] API_SECRET loaded:", API_SECRET[:6], "***")
 client = Client(API_KEY, API_SECRET)
 client.FUTURES_URL = 'https://fapi.binance.com/fapi'
 
